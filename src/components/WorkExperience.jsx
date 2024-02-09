363e50927abadb1015fd5c472ebdc1872122afad
Rishav Uppal <rishuuppal0123@gmail.com>
2024-02-09 14:54:01 +0530

    started work section

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const WorkExperience = () => {
   return (
-    <div>WorkExperience</div>
+    <div
+      name="workExperience"
+      className="bg-gradient-to-b from-gray-800 to-black w-full"
+    >
+      {/* <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div style={styles.experience}>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Work Experience
+          </p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+          
+        </div>
+      </div> */}
+    </div>
   )
 }
 
